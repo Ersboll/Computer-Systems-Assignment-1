14f3ec2fd5cf04fe1b42805dd5cb8ccab59d931f
Niels Kjær Ersbøll <nker@dtu.dk>
2021-09-29 15:26:50 +0200

    removed maximum deviation

--- src/threshold.c
+++ src/threshold.c
@@ -5,8 +5,6 @@ void binary_threshold(unsigned char (*in_image_buffer)[BMP_HEIGTH],
 
 #if THRESHOLDING_METHOD == OTSU
   unsigned char threshold = _otsu(in_image_buffer);
-#elif THRESHOLDING_METHOD == MAXIMUM_DEVIATION
-  unsigned char threshold = _maximum_deviation(in_image_buffer);
 #else
   unsigned char threshold = BINARY_THRESHOLD_VALUE;
 #endif
@@ -22,7 +20,7 @@ void binary_threshold(unsigned char (*in_image_buffer)[BMP_HEIGTH],
   }
 }
 
-#if THRESHOLDING_METHOD == OTSU || THRESHOLDING_METHOD == MAXIMUM_DEVIATION
+#if THRESHOLDING_METHOD == OTSU
 
 void _compute_histogram(unsigned int *histogram,
                         unsigned char (*in_image_buffer)[BMP_HEIGTH]) {
@@ -87,37 +85,3 @@ unsigned char _otsu(unsigned char (*in_image_buffer)[BMP_HEIGTH]) {
   return threshold;
 }
 #endif // THRESHOLDING_METHOD == OTSU
-
-#if THRESHOLDING_METHOD == MAXIMUM_DEVIATION
-unsigned char _maximum_deviation(unsigned char (*in_image_buffer)[BMP_HEIGTH]) {
-
-  unsigned short histogram[HISTOGRAM_SIZE];
-  unsigned char threshold;
-
-  _compute_histogram(histogram, in_image_buffer);
-
-  unsigned char peak_location = 0;
-  unsigned long peak_value = 0;
-
-  for (unsigned short i = 0; i < HISTOGRAM_SIZE; i++) {
-    if (histogram[i] >= peak_value) {
-      peak_location = i;
-      peak_value = histogram[i];
-    }
-  }
-
-  // maximum deviation
-  float maximum_deviation, val;
-  for (unsigned short i = peak_location + 1; i < HISTOGRAM_SIZE; i++) {
-    val = (float)(HISTOGRAM_SIZE - i) / (HISTOGRAM_SIZE - peak_location) *
-              peak_value -
-          histogram[i];
-    if (val > maximum_deviation) {
-      maximum_deviation = val;
-      threshold = i;
-    }
-  }
-
-  return threshold;
-}
-#endif
--- src/threshold.h
+++ src/threshold.h
@@ -12,7 +12,6 @@ void binary_threshold(unsigned char (*in_image_buffer)[BMP_HEIGTH],
                       unsigned char (*out_image_buffer)[BMP_HEIGTH]);
 
 unsigned char _otsu(unsigned char (*in_image_buffer)[BMP_HEIGTH]);
-unsigned char _maximum_deviation(unsigned char (*in_image_buffer)[BMP_HEIGTH]);
 void _compute_histogram(unsigned int *histogram,
                         unsigned char (*in_image_buffer)[BMP_HEIGTH]);
 
